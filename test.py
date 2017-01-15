3ffa6bb9546a72f73bb683c314c7de8df18281dd
Sainath <iamsainath2@gmail.com>
2017-01-15 16:15:57 +0530

    write tweets and sent to csv

---
+++ test.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+# my_dict = {'name': ['sainath', 'sreenath'], 'age': [27,25]}
+# print(my_dict)
+# df = pd.DataFrame(my_dict)
+# print(df)
+
+my_dict = {'number': []}
+
+for i  in range(10):
+	my_dict['number'].append(i)
+
+print(my_dict)
--- twitter_sentiment.py
+++ twitter_sentiment.py
@@ -1,11 +1,22 @@
 import tweepy
 from textblob import TextBlob
+import pandas as pd
+import os
 
-consumer_key = "iguIihI2T2LstZtTaihLKjnrY"
-consumer_secret = "RAyfOKivniDu2XlGMx2adCsiDTg17kldvbOcJOA6iThG3BGHTY"
+consumer_key = ""
+consumer_secret = ""
 
-access_token = "742220055002243072-hyS8kunykG2eBaQA7qhJfDn0WzY7Dg9"
-access_token_secret = "kKHhFVzzw4NvhmMKFInLOwHrlQkVctA5xzcqYLtYXf2gB"
+access_token = ""
+access_token_secret = ""
+
+def check_sentiment(number):
+	if number > 0:
+		number = 'positive'
+	elif number < 0:
+		number = 'negative'
+	else:
+		number = 'neutral'
+	return number
 
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
@@ -14,9 +25,38 @@ api = tweepy.API(auth)
 
 public_tweets = api.search("trump")
 
+data_dict = {'tweet': [], 'sentiment': []}
+
 for tweet in public_tweets:
-	print(tweet.text)
+	# print(tweet.text)
+	# print(" ")
+	# print(" ")
 	analysis = TextBlob(tweet.text)
-	print(analysis.sentiment)
-	print(" ")
+	# print(" ")
+	# print(analysis.sentiment.polarity)
+	# print(" ")
+	sentiment = check_sentiment(analysis.sentiment.polarity)
+	# print(sentiment)
+	data_dict['tweet'].append(tweet.text) 
+	data_dict['sentiment'].append(sentiment)
+
+# # print(data_dict)
+# print(pd.DataFrame(data_dict))
+
+
+data_dict = pd.DataFrame(data_dict)
+# print(data_dict)
+data_dict = data_dict[['tweet', 'sentiment']]
+# print(os.getcwd())
+data_dict.to_csv("sentiment_tweets.csv", index = False)
+
+
+
+
+
+
+
+
+
+
 
